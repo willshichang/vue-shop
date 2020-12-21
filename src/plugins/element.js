6e6b5b4788430300606c51777e9b004e2c383018
willshichang <1401299775@qq.com>
2020-12-21 18:38:09 +0800

    新增退出功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+//  导入弹窗提示组件并挂载在VUE上
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
